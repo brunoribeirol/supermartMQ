13794d8bb86270e83c8774e7d8efd217bf96c5d5
Bruno Ribeiro <araujoribeiro.bruno@gmail.com>
2025-03-31 19:12:38 -0300

    chore: "Testing the integration Python-Java"

---
+++ src/main/java/com/SupermartMQ/SupermartMqApplication.java
@@ -0,0 +1,13 @@
+package com.SupermartMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermartMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermartMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SupermartMQ/SupermartMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SupermartMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermartMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
